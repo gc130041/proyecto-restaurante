bfd977f5df1ba47faeb47ee74b46b0461d43666d
SERV-DRV <sderosa-2023220@kinal.edu.gt>
2026-02-16 08:04:44 -0600

    fix: Ruta

--- server-admin/src/users/user.routes.js
+++ server-admin/src/users/user.routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { check } from 'express-validator';
 import { login, register, getUsers, getProfile } from './user.controller.js';
-import { registerValidator, loginValidator } from '../../middlewares/user-validators.js';
+import { registerValidator, loginValidator } from '../../middlewares/users-validators.js';
 import { validateJWT } from '../../middlewares/validate-jwt.js';
 
 const router = Router();
